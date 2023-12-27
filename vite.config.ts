ae018aa6f7d2e19ad286da9ff6930ba9366f11fc
Tirth Kapadi <mr.t.k.patel@gmail.com>
2023-12-27 21:57:10 +0530

    Vite Config Changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-	server: {
-		proxy: {
-			"/api": "http://localhost:8000",
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	return defineConfig({
+		server: {
+			proxy: {
+				"/api": process.env.VITE_BACKEND_URL,
+			},
+			host: "0.0.0.0",
 		},
-		host: "0.0.0.0",
-	},
-	plugins: [react()],
-});
+		plugins: [react()],
+	})
+};
